41522a903cddcf954d6c4f60cf8e4ea57bae65c5
prab1999 <prabhatgautam1306@gmail.co>
2019-08-10 19:03:25 +0530

    after adding progress bar,net check

--- app/src/main/java/com/example/cp/Dashboard.java
+++ app/src/main/java/com/example/cp/Dashboard.java
@@ -8,6 +8,8 @@ import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
@@ -22,6 +24,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class Dashboard extends Fragment {
@@ -32,6 +36,23 @@ public class Dashboard extends Fragment {
     String siteUrl;
     Context m;
     ArrayList<String> tags=new ArrayList<String>();
+    boolean netconnected=false;
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) m.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
     public void changes(){
         Grid gd=(Grid)getFragmentManager().findFragmentById(R.id.fragment2);
         if(!tags.isEmpty()){
@@ -42,6 +63,7 @@ public class Dashboard extends Fragment {
                 toast.setMargin(50,50);
                 toast.show();
                 tags.clear();
+                but.setEnabled(true);
             }
         }
     }
@@ -66,12 +88,44 @@ public class Dashboard extends Fragment {
                 }
 
                 siteUrl = "https://codeforces.com/problemset/problem/"+c.substring(0,ind)+"/"+c.substring(ind);
+
+                but.setEnabled(false);
+                (new Checknet()).execute();
                 (new ParseURL() ).execute();
 
 
         }});
 
         return view;
+    }
+    private class Checknet extends AsyncTask<Void,Void,Void>{
+
+        boolean initialstate;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            initialstate=haveNetworkConnection();
+            netconnected=initialstate;
+            but.setEnabled(false);
+        }
+
+        protected Void doInBackground(Void... voids) {
+            while(!netconnected){
+             netconnected=haveNetworkConnection();}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            but.setEnabled(true);
+            if(initialstate==false){
+            Toast toast=Toast.makeText(getActivity(),"Internet Connected",Toast.LENGTH_SHORT);
+            toast.setMargin(150,150);
+            toast.show();}
+        }
     }
     private class ParseURL extends AsyncTask<Void, Void, Void> {
         ProgressBar progressBar=(ProgressBar)getActivity().findViewById(R.id.progressBar1);
@@ -87,15 +141,21 @@ public class Dashboard extends Fragment {
         protected Void doInBackground(Void... params) {
 
             try {
-                // Connect to the web site
                 Document document = Jsoup.connect(siteUrl).get();
+                System.out.println(document.empty());
+                Elements em=document.select("h3");
+                String wr=em.first().text();
                 Elements elems=document.select("span.tag-box");
                 for(Element tag:elems){
                     tags.add(tag.text());
                     System.out.println(tags.get(tags.size()-1));
                 }
-                tags.remove(tags.size()-1);
+                if(tags.size()>0){
+                tags.remove(tags.size()-1);}
 
+            }
+            catch(UnknownHostException e){
+                System.out.println("wrong");
             } catch (IOException e) {
                 e.printStackTrace();
             }
